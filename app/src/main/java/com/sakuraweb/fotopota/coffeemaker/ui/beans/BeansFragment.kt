e94ac863faeaaa87b33f3529363aedd2fa9e1dd5
Shiro32 <dm@garneau.sakura.ne.jp>
2020-06-07 15:00:24 +0900

    V0.0 メニューボタンを合わせただけ home, beans, settings

---
+++ app/src/main/java/com/sakuraweb/fotopota/coffeemaker/ui/beans/BeansFragment.kt
@@ -0,0 +1,31 @@
+package com.sakuraweb.fotopota.coffeemaker.ui.beans
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.sakuraweb.fotopota.coffeemaker.R
+
+class BeansFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: BeansViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(BeansViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_beans, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
